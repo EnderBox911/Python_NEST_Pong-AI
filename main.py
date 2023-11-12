fb8de3712da3c7296f2929f726b0c83579e031c4
Akmal <akmalyahaya305@gmail.com>
2023-11-12 23:55:22 +0300

    Fixed doc formats

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ screen_height = 600
 
 screen = pyg.display.set_mode((screen_width, screen_height))
 
-player = pyg.Rect((300,250,50,50)) #x,y,width,height
+player = pyg.Rect((300, 250, 50, 50))  # x,y,width,height
 
 run = True
 while run:
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
 
-    pyg.draw.rect(screen, (255,0,0), player) #screen,rgb,object
+    pyg.draw.rect(screen, (255, 0, 0), player)  # screen,rgb,object
 
     key = pyg.key.get_pressed()
     if key[pyg.K_a]:
-        player.move_ip(-1,0) #x,y
+        player.move_ip(-1, 0)  # x,y
     if key[pyg.K_d]:
-        player.move_ip(1,0) #x,y
+        player.move_ip(1, 0)  # x,y
     if key[pyg.K_w]:
-        player.move_ip(0,-1) #x,y
+        player.move_ip(0, -1)  # x,y
     if key[pyg.K_s]:
-        player.move_ip(0,1) #x,y
-
+        player.move_ip(0, 1)  # x,y
 
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
