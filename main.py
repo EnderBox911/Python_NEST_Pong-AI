00f662ba54a6ca3710b02e1f75e26635acee2445
Akmal <akmalyahaya305@gmail.com>
2023-11-12 23:39:33 +0300

    Add basic code

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-impr
+import pygame as pyg
+
+pyg.init()
+
+screen_width = 800
+screen_height = 600
+
+screen = pyg.display.set_mode((screen_width, screen_height))
+
+run = True
+while run:
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            run = False
+
+pyg.quit()
