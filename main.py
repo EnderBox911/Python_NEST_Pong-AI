b4b3d75f2b4a5b4b0861963c573cad03db16a8b3
Akmal <akmalyahaya305@gmail.com>
2023-11-14 21:28:43 +0300

    Basic structural changes

--- main.py
+++ main.py
@@ -59,55 +59,66 @@ class Paddle:
         pyg.draw.rect(screen, white, self.rect)
 
     def ai(self):
+        # Placeholder AI
+        # + randint for some variation
         if self.rect.centery < pong.rect.top and self.rect.bottom < screen_height:
-            self.rect.move_ip(0, self.speed + (randint(1,3)) )
+            self.rect.move_ip(0, self.speed + (randint(1,3)))
         if self.rect.centery > pong.rect.bottom and self.rect.top > margin:
-            self.rect.move_ip(0, -1 * self.speed - (randint(1,3)) )
+            self.rect.move_ip(0, -1 * self.speed - (randint(1,3)))
 
 
 class Ball():
     def __init__(self, x, y):
         self.reset(x, y)
 
+    def reset(self, x, y):
+        self.x = x # Start position
+        self.y = y # Start position
+        self.radius = 8
+        self.rect = pyg.Rect((self.x, self.y, self.radius * 2, self.radius * 2))
+        self.speedX = -5
+        self.speedY = 5 # Initial Y velocity of the ball
+        self.max_Y_vel = self.speedY
+        self.winner = 0  # 0 = player, -1 = ai
+
     def move(self):
-        if self.rect.top < margin:
+        # Top and bottom border check
+        if self.rect.top < margin or self.rect.bottom > screen_height:
             self.speedY *= -1
 
-        if self.rect.bottom > screen_height:
-            self.speedY *= -1
-
-        ####
-        # Add proper collision detection HERE
-        
+        # Check for paddle collision
         if self.rect.colliderect(player_paddle) or self.rect.colliderect(ai_paddle):
             self.speedX *= -1
 
+            # Getting which paddle collided
             paddle = player_paddle if self.rect.colliderect(player_paddle) else ai_paddle
 
-            # Middle of paddle
+            # Finding middle of paddle
             middle_y = paddle.rect.y + paddle.height / 2
 
             # Difference in Y axis of ball to middle of paddle
             diff_in_y = middle_y - self.rect.y
 
-            # Farther from center of paddle = the angle of y change is greater. When the ball hits the edge of the
-            # paddle, it reaches the maximum velocity
-            reducing_factor = (paddle.height/2)/self.max_vel
+            # Farther from center of paddle = the Y velocity is greater. When the ball hits the edge of the
+            # paddle, it reaches the maximum velocity. Finding the amount of Y velocity for each unit paddle length,
+            # meaning that if the ball hits the edge (the full length) of the paddle, it gives the maximum Y velocity
+            reducing_factor = (paddle.height/2) / self.max_Y_vel
 
             # Squeezes the difference in y to the range of the max velocity
             y_vel = diff_in_y/reducing_factor
 
             # Makes that the y velocity
             self.speedY = -1 * y_vel
-            
-        ####
 
+        # Passes left side, user wins round
         if self.rect.left < 0:
             self.winner = 1
 
+        # Passes right side, ai wins round
         if self.rect.right > screen_width:
             self.winner = -1
 
+        # Adds speeds to ball
         self.rect.x += self.speedX
         self.rect.y += self.speedY
 
@@ -116,28 +127,19 @@ class Ball():
     def draw(self):
         pyg.draw.circle(screen, white, (self.rect.x + self.radius, self.rect.y + self.radius), self.radius)
 
-    def reset(self, x, y):
-        self.x = x
-        self.y = y
-        self.radius = 8
-        self.rect = pyg.Rect((self.x, self.y, self.radius * 2, self.radius * 2))
-        self.speedX = -5
-        self.speedY = 5
-        self.max_vel = 5
-        self.winner = 0  # 0 = player, -1 = ai
 
-
+# Instantiate the paddles and pong
 player_paddle = Paddle(screen_width - 40, screen_height // 2)
 ai_paddle = Paddle(20, screen_height // 2)
-
 pong = Ball(screen_width - 60, screen_height // 2 + 50)
 
 
 run = True
 while run:
-
+    # Ensures this loop runs 60 times per second
     clock.tick(FPS)
 
+    # Set board up
     draw_board()
     draw_text('AI: ' + str(ai_score), font, white, 20, 15)
     draw_text('Player: ' + str(player_score), font, white, screen_width -125, 15)
@@ -187,10 +189,10 @@ while run:
 
         if pong.speedY < 0:
             pong.speedY -= 1
-            pong.max_vel += 1
+            pong.max_Y_vel += 1
         if pong.speedY > 0:
             pong.speedY += 1
-            pong.max_vel += 1
+            pong.max_Y_vel += 1
 
 
     pyg.display.update()
