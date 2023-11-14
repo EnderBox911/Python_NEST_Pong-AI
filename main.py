84bab1531aac068747b2a77d208388f8a1c5e397
Akmal <akmalyahaya305@gmail.com>
2023-11-14 22:14:11 +0300

    Minor reformatting

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ pyg.init()
 bg = (50, 25, 50)
 white = (255, 255, 255)
 
+
 class Paddle:
     def __init__(self, x, y):
-        self.x = x # Start position
-        self.y = y # Start position
+        self.x = x  # Start position
+        self.y = y  # Start position
         self.width = 20
         self.height = 100
         self.rect = pyg.Rect((self.x, self.y, self.width, self.height))
@@ -31,9 +32,9 @@ class Paddle:
         # Placeholder AI
         # + randint for some variation
         if self.rect.centery < pong.rect.top and self.rect.bottom < screen_height:
-            self.rect.move_ip(0, self.speed + (randint(1,3)))
+            self.rect.move_ip(0, self.speed + (randint(1, 3)))
         if self.rect.centery > pong.rect.bottom and self.rect.top > margin:
-            self.rect.move_ip(0, -1 * self.speed - (randint(1,3)))
+            self.rect.move_ip(0, -1 * self.speed - (randint(1, 3)))
 
 
 class Ball():
@@ -41,12 +42,12 @@ class Ball():
         self.reset(x, y)
 
     def reset(self, x, y):
-        self.x = x # Start position
-        self.y = y # Start position
+        self.x = x  # Start position
+        self.y = y  # Start position
         self.radius = 8
         self.rect = pyg.Rect((self.x, self.y, self.radius * 2, self.radius * 2))
         self.speedX = -5
-        self.speedY = 5 # Initial Y velocity of the ball
+        self.speedY = 5  # Initial Y velocity of the ball
         self.max_Y_vel = self.speedY
         self.winner = 0  # 0 = player, -1 = ai
 
@@ -71,10 +72,10 @@ class Ball():
             # Farther from center of paddle = the Y velocity is greater. When the ball hits the edge of the
             # paddle, it reaches the maximum velocity. Finding the amount of Y velocity for each unit paddle length,
             # meaning that if the ball hits the edge (the full length) of the paddle, it gives the maximum Y velocity
-            reducing_factor = (paddle.height/2) / self.max_Y_vel
+            reducing_factor = (paddle.height / 2) / self.max_Y_vel
 
             # Squeezes the difference in y to the range of the max velocity
-            y_vel = diff_in_y/reducing_factor
+            y_vel = diff_in_y / reducing_factor
 
             # Makes that the y velocity
             self.speedY = -1 * y_vel
@@ -209,106 +210,6 @@ class Game:
         pyg.quit()
 
 
-"""
-screen_width = 600
-screen_height = 500
-
-clock = pyg.time.Clock()
-
-screen = pyg.display.set_mode((screen_width, screen_height))
-pyg.display.set_caption('Pong')
-
-
-font = pyg.font.SysFont('Constantia', 30)
-
-live_ball = False
-margin = 50
-ai_score = 0
-player_score = 0
-FPS = 60
-winner = 0
-speed_increase = 0
-
-
-
-def draw_board():
-    screen.fill(bg)
-    pyg.draw.line(screen, white, (0, margin), (screen_width, margin))
-
-
-def draw_text(text, font, colour, x, y):
-    img = font.render(text, True, colour)
-    screen.blit(img, (x, y))
-"""
-
-
-
-"""
-run = True
-while run:
-    # Ensures this loop runs 60 times per second
-    clock.tick(FPS)
-
-    # Set board up
-    draw_board()
-    draw_text('AI: ' + str(ai_score), font, white, 20, 15)
-    draw_text('Player: ' + str(player_score), font, white, screen_width -125, 15)
-    draw_text('BALL SPEED: ' + str(abs(pong.speedX)), font, white, screen_width // 2 - 100, 15)
-
-    player_paddle.draw()
-    ai_paddle.draw()
-
-    if live_ball:
-        speed_increase += 1
-        winner = pong.move()
-        if winner == 0:
-            player_paddle.move()
-            ai_paddle.ai()
-            pong.draw()
-        else:
-            live_ball = False
-            if winner == 1:
-                player_score += 1
-            else:
-                ai_score += 1
-
-    if not live_ball:
-        if winner == 0:
-            draw_text('CLICK ANYWHERE TO START', font, white, 100, screen_height // 2 - 100)
-        if winner == 1:
-            draw_text('YOU SCORED!', font, white, 220, screen_height // 2 - 100)
-            draw_text('CLICK ANYWHERE TO START', font, white, 100, screen_height // 2 - 50)
-        if winner == -1:
-            draw_text('AI SCORED!', font, white, 220, screen_height // 2 - 100)
-            draw_text('CLICK ANYWHERE TO START', font, white, 100, screen_height // 2 - 50)
-
-
-    for event in pyg.event.get():
-        if event.type == pyg.QUIT:
-            run = False
-        if event.type == pygame.MOUSEBUTTONDOWN and not live_ball:
-            live_ball = True
-            pong.reset(screen_width - 60, screen_height // 2 + 50)
-
-    if speed_increase > 500:
-        speed_increase = 0
-        if pong.speedX < 0:
-            pong.speedX -= 1
-        if pong.speedX > 0:
-            pong.speedX += 1
-
-        if pong.speedY < 0:
-            pong.speedY -= 1
-            pong.max_Y_vel += 1
-        if pong.speedY > 0:
-            pong.speedY += 1
-            pong.max_Y_vel += 1
-
-
-    pyg.display.update()
-
-pyg.quit()
-"""
-
-game = Game()
-game.start_game()
+if __name__ == "__main__":
+    game = Game()
+    game.start_game()
